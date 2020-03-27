36ee3b2350d1079a9dc66cf33baaf657824ce8ad
Hitallo William <hitallo@viggosistemas.com.br>
2020-03-26 21:37:32 -0300

    instalando knex e sqlite

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const app = require("express")();
+const express = require('express');
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Ops" });
+const app = express();
+
+app.use(express.json());
+
+app.post('/user', (request, response) => {
+  console.log(request.body);
+  return response.json({ message: 'Ops' });
+});
+
+app.get('/user/:id', (request, response) => {
+  const body = request.body;
+  const params = request.query;
+  console.log(body);
+  response.json({
+    id: request.params.id,
+    nome: params.nome
+  });
 });
 
 app.listen(3333, () => {
-  console.log("Servidor ouvindo");
+  console.log('Servidor ouvindo');
 });
